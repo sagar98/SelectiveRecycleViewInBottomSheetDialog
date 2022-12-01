e8d6b6e4d03a6c4cc0f648d98beb5d7f99b7ecaa
AkshayBJS <anighot@bjsindia.org>
2022-12-01 17:40:39 +0530

    test commit

--- app/src/main/java/com/sagar/selectiverecycleviewinbottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/sagar/selectiverecycleviewinbottomsheetdialog/MainActivity.kt
@@ -60,8 +60,6 @@ class MainActivity : AppCompatActivity(), CustomBottomSheetDialogInterface {
                 SelectionListObject("14", "City8", false),
                 SelectionListObject("15", "City5", false),
                 SelectionListObject("16", "City6", false),
-                SelectionListObject("17", "City7", false),
-
             )
         )
 
