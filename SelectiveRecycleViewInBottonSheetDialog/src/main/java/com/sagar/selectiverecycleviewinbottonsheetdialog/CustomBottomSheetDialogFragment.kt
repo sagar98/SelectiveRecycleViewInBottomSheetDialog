5937a3cea7ece6a73a0054a3d77bc51800affab6
AkshayBJS <anighot@bjsindia.org>
2022-12-05 12:13:43 +0530

    Updated BottomSheetDialog to BottomSheetDialogFragment to get access to onDismiss callback

--- SelectiveRecycleViewInBottonSheetDialog/src/main/java/com/sagar/selectiverecycleviewinbottonsheetdialog/CustomBottomSheetDialogFragment.kt
+++ SelectiveRecycleViewInBottonSheetDialog/src/main/java/com/sagar/selectiverecycleviewinbottonsheetdialog/CustomBottomSheetDialogFragment.kt
@@ -1,30 +1,27 @@
 package com.sagar.selectiverecycleviewinbottonsheetdialog
 
-import android.app.Activity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.button.MaterialButton
 import com.sagar.selectiverecycleviewinbottonsheetdialog.adapter.BottomsheetAdapter
-import com.sagar.selectiverecycleviewinbottonsheetdialog.model.SelectionListObject
 import com.sagar.selectiverecycleviewinbottonsheetdialog.interfaces.CustomBottomSheetDialogInterface
+import com.sagar.selectiverecycleviewinbottonsheetdialog.model.SelectionListObject
 
-class CustomBottomSheetDialogClass(
-    activity: Activity, listenerContext: CustomBottomSheetDialogInterface,
-    private var title: String, selectionList: ArrayList<SelectionListObject>,
-    isMultiSelectAllowed: Boolean
-) :
-    BottomSheetDialog(activity), View.OnClickListener {
+class CustomBottomSheetDialogFragment(
+    listenerContext: CustomBottomSheetDialogInterface, private var title: String,
+    selectionList: ArrayList<SelectionListObject>, isMultiSelectAllowed: Boolean
+) : BottomSheetDialogFragment(), View.OnClickListener {
 
     private var bottomsheetTitle: TextView? = null
 
-    // private lateinit var imgClose: ImageView
     private lateinit var btnApply: MaterialButton
     private lateinit var btnClose: MaterialButton
     private lateinit var btnClearAll: MaterialButton
@@ -34,35 +31,53 @@ class CustomBottomSheetDialogClass(
     private var listenerContext: CustomBottomSheetDialogInterface
     private var isMultiSelectAllowed: Boolean = false
 
+    companion object{
+        const val TAG = "BottomSheetFragment"
+    }
+
     init {
         this.listenerContext = listenerContext
         this.selectionList = selectionList
         this.isMultiSelectAllowed = isMultiSelectAllowed
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottomsheetdialog_layout, container, false)
+    }
 
-        val bottomSheetView: View = layoutInflater.inflate(R.layout.bottomsheetdialog_layout, null)
-        setContentView(bottomSheetView)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         val bottomSheetBehavior: BottomSheetBehavior<*> =
-            BottomSheetBehavior.from(bottomSheetView.parent as View)
+            BottomSheetBehavior.from(view.parent as View)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
 
-        btnApply = findViewById(R.id.btn_apply)!!
-        btnClose = findViewById(R.id.btn_cancel)!!
-        btnClearAll = findViewById(R.id.bt_clear_all)!!
+        btnApply = view.findViewById(R.id.btn_apply)!!
+        btnClose = view.findViewById(R.id.btn_cancel)!!
+        btnClearAll = view.findViewById(R.id.bt_clear_all)!!
         btnApply.setOnClickListener(this)
         btnClose.setOnClickListener(this)
         btnClearAll.setOnClickListener(this)
-        bottomsheetTitle = findViewById(R.id.text_title)
+        bottomsheetTitle = view.findViewById(R.id.text_title)
         bottomsheetTitle!!.text = title
 
-        rvBottomSheet = findViewById(R.id.rv_bottomsheet_dialog)!!
-        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(ownerActivity)
+        rvBottomSheet = view.findViewById(R.id.rv_bottomsheet_dialog)!!
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
         rvBottomSheet.layoutManager = layoutManager
         bottomsheetAdapter = BottomsheetAdapter(selectionList, isMultiSelectAllowed)
         rvBottomSheet.adapter = bottomsheetAdapter
+
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        for (i in selectionList.indices) {
+            selectionList[i].isNewlySelected = selectionList[i].isSelected
+        }
+        super.onDismiss(dialog)
     }
 
     override fun onClick(view: View?) {
